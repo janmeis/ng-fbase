e367f8ded9e1bc55045e66944d597dec79d266df
Jan Meissner <janmeis@yahoo.co.uk>
2020-03-21 16:25:30 +0100

    update ng bootstrap

--- src/app/services/authentication-service.ts
+++ src/app/services/authentication-service.ts
@@ -10,9 +10,28 @@ import { first } from 'rxjs/operators';
 })
 export class AuthenticationService {
   user$: Observable<firebase.User>;
+  sessionTimeout: any;
 
   constructor(private angularFireAuth: AngularFireAuth) {
     this.user$ = angularFireAuth.authState;
+    // angularFireAuth.auth.onAuthStateChanged((user) => {
+    //   if (user == null) {
+    //     // User is logged out.
+    //     // Clear the session timeout.
+    //     clearTimeout(this.sessionTimeout);
+    //     this.sessionTimeout = null;
+    //   } else {
+    //     // User is logged in.
+    //     // Fetch the decoded ID token and create a session timeout which signs the user out.
+    //     user.getIdTokenResult().then((idTokenResult) => {
+    //       // Make sure all the times are in milliseconds!
+    //       const authTime = idTokenResult.claims.auth_time * 1000;
+    //       const sessionDuration = 1000 * 60 * 60 * 24 * 30;
+    //       const millisecondsUntilExpiration = sessionDuration - (Date.now() - authTime);
+    //       this.sessionTimeout = setTimeout(() => angularFireAuth.auth.signOut(), millisecondsUntilExpiration);
+    //     });
+    //   }
+    // });
   }
 
   SignIn$ = (email: string, password: string): Observable<firebase.auth.UserCredential> =>
